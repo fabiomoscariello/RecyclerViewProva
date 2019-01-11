9df59b5d9c359b0022accfea8b29ded725f4db81
Lenovo-PC\Fabio Moscariello <mf.92@hotmail.it>
2019-01-11 15:21:50 +0100

    Aggiunta log bello

--- app/src/main/java/com/example/fabiomoscariello/recyclerviewprova/MainActivity.java
+++ app/src/main/java/com/example/fabiomoscariello/recyclerviewprova/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listaAnimali = new ArrayList<Animale>();
-        Log.d(TAG,"PRova");
+        Log.d(TAG,"PRova bello");
         listaAnimali.add(new Animale("Cane"));
         listaAnimali.add(new Animale("Gatto"));
         listaAnimaliView = findViewById(R.id.listaAnimaliView_id);
